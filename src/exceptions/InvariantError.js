16a57e609e1e18560413dfb44450efbd4ad60f14
mutiara <mutiaranis17@gmail.com>
2021-08-07 18:48:27 +0700

    data validation implemented

---
+++ src/exceptions/InvariantError.js
@@ -0,0 +1,10 @@
+const ClientError = require('./ClientError');
+
+class InvariantError extends ClientError {
+    constructor(message) {
+        super(message);
+        this.name = 'InvariantError';
+    }
+}
+// ga perlu menetapkan status code karena default turunan ClientError nilainya 400
+module.exports = InvariantError;
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 const Hapi = require('@hapi/hapi');
-const routes = require('./routes');
+const notes = require('./api/notes');
+const NotesService = require('./services/inMemory/NotesService');
+const NotesValidator = require('./validator/notes');
 
 const init = async () => {
+    const notesService = new NotesService();
     const server = Hapi.server({
         port: 5001,
         host: process.env.NODE_ENV !== 'production' ? 'localhost' : '172.31.37.145',
@@ -12,7 +15,13 @@ const init = async () => {
         },
     });
 
-    server.route(routes);
+    await server.register({
+        plugin: notes,
+        options: {
+            service: notesService,
+            validator: NotesValidator,
+        },
+    });
 
     await server.start();
     console.log(`Server berjalan pada ${server.info.uri}`);
